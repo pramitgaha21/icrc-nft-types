faee161a350727c67e11b081f950ce1646529108
Pramit Gaha <pramitgaha21@protonmail.com>
2024-04-02 02:32:34 +0545

    version fix and re-export of account

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
-pub mod icrc7;
-pub mod icrc37;
-pub mod mintable;
 pub mod burnable;
+pub mod icrc37;
+pub mod icrc7;
+pub mod mintable;
+
+// rexport
+pub use icrc_ledger_types::icrc1::account::{Account, Subaccount, DEFAULT_SUBACCOUNT};
+
