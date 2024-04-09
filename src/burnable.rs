e7e4c06fb210129415d5221edfbe4b39d52245e8
Pramit Gaha <pramitgaha21@protonmail.com>
2024-04-10 01:03:04 +0545

    fix

--- src/burnable.rs
+++ src/burnable.rs
@@ -1,10 +1,13 @@
+use candid::CandidType;
 use icrc_ledger_types::icrc1::account::Account;
+use serde::{Deserialize, Serialize};
 
-pub enum BurnError{
+#[derive(CandidType, Serialize, Deserialize, Debug)]
+pub enum BurnError {
     Unauthorized,
-    GenericError{ error_code: u128, message: String },
+    GenericError { error_code: u128, message: String },
 }
-pub trait Burnable{
+pub trait Burnable {
     type BurnArg;
 
     fn is_burnable(&self) -> bool;
