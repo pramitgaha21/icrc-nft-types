241894c5551ae62d54336abb9f76557db21a8dba
Pramit Gaha <pramitgaha21@protonmail.com>
2024-05-12 12:02:52 +0545

    encode block

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,31 @@ pub mod icrc37;
 pub mod icrc7;
 pub mod mintable;
 
+use candid::CandidType;
 // rexport
 pub use icrc_ledger_types::icrc1::account::{Account, Subaccount, DEFAULT_SUBACCOUNT};
+use serde::{Deserialize, Serialize};
+use serde_bytes::ByteBuf;
 
+#[derive(
+    CandidType, Serialize, Deserialize, Debug, Clone, Eq, PartialEq, Ord, PartialOrd, Hash,
+)]
+pub struct EncodedBlock(pub ByteBuf);
+
+impl EncodedBlock {
+    pub fn from_vec(bytes: Vec<u8>) -> Self {
+        Self(ByteBuf::from(bytes))
+    }
+
+    pub fn into_vec(self) -> Vec<u8> {
+        self.0.to_vec()
+    }
+
+    pub fn as_slice(&self) -> &[u8] {
+        &self.0
+    }
+
+    pub fn size_bytes(&self) -> usize {
+        self.0.len()
+    }
+}
